725c46f906f97d04e43e1c4077909771d7d27b97
torden81 <torden81@users.noreply.github.com>
2023-07-29 12:33:21 +0200

    added css

--- dev/02_dev.R
+++ dev/02_dev.R
@@ -32,7 +32,7 @@ golem::add_fct("physics", with_test = TRUE)
 ## Creates .js and .css files at inst/app/www
 #golem::add_js_file("script")
 #golem::add_js_handler("handlers")
-#golem::add_css_file("custom")
+golem::add_css_file("custom")
 #golem::add_sass_file("custom")
 
 ## Add internal datasets ----
